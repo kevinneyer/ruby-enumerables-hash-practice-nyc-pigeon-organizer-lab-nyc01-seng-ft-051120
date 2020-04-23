1fdcdeca860706b23c9add383fba006262fc1190
Kevin Neyer <kevinneyer@gmail.com>
2020-04-23 18:34:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,10 +4,10 @@ def nyc_pigeon_organizer(data)
   # write your code here!
 final = {}
 
-data.each do |attribute, attribute_name|
+data.each do |attribute|
   binding.pry
-  attribute_name.each do |style, name_array|
-    #binding.pry
+  attribute[:gender].each do |style, name_array|
+    binding.pry
     name_array.each do |name|
       final[name] = {}
       final[name][attribute][attribute_name]
