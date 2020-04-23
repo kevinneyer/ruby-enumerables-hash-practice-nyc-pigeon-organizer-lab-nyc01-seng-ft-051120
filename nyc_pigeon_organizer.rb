b87895253f03e6ed1541f6458459cd01be89b798
Kevin Neyer <kevinneyer@gmail.com>
2020-04-23 18:17:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,24 +2,5 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   # write your code here!
-
-final = {}
-
-data.each do |attribute_name, attribute|
-  binding.pry 
-    attribute.each do |style, names_array|
-      binding.pry 
-      names_array.each do |name|
-        binding.pry 
-        if final[name] == nil 
-           final[name] = {}
-        end
-        if final[name][attribute_name] == nil 
-           final[name][attribute_name] = []
-        end
-        final[name][attribute_name].push(style.to_s)
-    end 
-  end
-end
-final
+data.each do |
 end
