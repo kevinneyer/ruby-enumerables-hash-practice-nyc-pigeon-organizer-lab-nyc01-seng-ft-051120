80fadb0b23eae6f0af36f307a0d9e15ce86565d7
Kevin Neyer <kevinneyer@gmail.com>
2020-04-23 18:25:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,13 +2,14 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   # write your code here!
+final = {}
 
 data.each do |attribute, attribute_name|
   #binding.pry
   attribute_name.each do |style, name_array|
     #binding.pry
     name_array.map do |name|
-      name.sort 
+      final[name]
 end
 end
 end 
